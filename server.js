b06622e0869075e8c03adc1a35a4f238ffadffbb
Sergey Plaski <splaski@gmail.com>
2018-09-04 16:18:47 +0300

    update 0904

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
