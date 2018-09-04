b6ae6b16fa918c7f432ba0f6af5cba0fcad70e76
Sergey Plaski <splaski@gmail.com>
2018-09-04 16:00:54 +0300

    update 0904

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ const server_port = config.serverConfig.port;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({ origin: '*' }));
+
+app.use(express.static(path.join(__dirname,'client/build')));
+
 app.use('/tasks', todoRoutes);
 app.use((req, res) =>  res.status(404).json({err: '404'}));
 app.use((err, req, res) => {console.log(err.stack); res.status(500).json({err: '500'})});
 
-
 mongoose.connect(DB, {useNewUrlParser: true })
     .then(() => console.log('Database is connected') , err => console.log('Can not connect to the database' +err));
+
+app.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
+});
 
 app.listen(server_port, () => console.log('Server is running on ', server_port));
